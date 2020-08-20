d788a25d48ae592d2c55ad87ac97153b2a11fd51
maxcroy1 <max.croy1@gmail.com>
2020-08-20 12:02:54 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            let index = state.friends.findIndex(friend => friend.id === action.id)
+            let allFriends = [...state.friends]
+            allFriends.splice(index, 1)
+            return  {friends: allFriends}
+        default:
+            return {friends: state.friends}
+    }
 }
