22d57c42a28bec913cebbc3b6ad989b89b6b8ee7
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-08-21 22:13:17 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,27 @@
-export function managePresents(state, action){
+import { render } from "react-dom";
+
+export function managePresents(state = {
+    numberOfPresents: 0,
+}, action){
+    switch(action.type){
+        case "INCREASE":
+        return Object.assign({}, state {
+            numberOfPresents: state.numberOfPresents+ 1 
+        })
+
+        default:
+            return state;
+
+    }
 }
+
+function render(){
+    document.body.textContent= state.count
+}
+
+function dispatch(action){
+    state = changeState(state, action)
+    render()
+}
+
+dispatch({ type: 'INCREASE_COUNT' })
